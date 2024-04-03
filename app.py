78007404dca1e62985ee6651ad36fccd01f6fd12
Fredin <90355410+fredinj@users.noreply.github.com>
2024-04-04 00:39:15 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -36,6 +36,10 @@ def scrape_market(search_term):
   
   market_list = []
   
+  flip_product = scrape_flipkart(encoded_term)
+  if flip_product and len(flip_product)>0:
+    market_list.append(flip_product)
+  
   md_product = scrape_md(encoded_term)
   if md_product and len(md_product)>0:  
     market_list.append(md_product)
@@ -72,39 +76,40 @@ def scrape_flipkart(encoded_term):
     # matched_element = flipkart_search_soup.find('a', class_="s1Q9rs")
     matched_element = flipkart_search_soup.find('a', href=lambda href: href and "/p/" in href)
     
-    flipkart_product_url = "https://www.flipkart.com" + matched_element['href']
-    
-    # print(flipkart_product_url)
-    
-    parsed_url = urlparse(flipkart_product_url)
-    query_params = parse_qs(parsed_url.query)
-    base_url = f"{parsed_url.scheme}://{parsed_url.netloc}{parsed_url.path}"
-    flipkart_parsed_url = f'{base_url}?q={encoded_term}'
-    
-    if query_params.get("spotlightTagId"):
-      flipkart_parsed_url += f'&spotlightTagId={query_params.get("spotlightTagId")[0]}'
-
-    if query_params.get("qH"):
-      flipkart_parsed_url += f'&qH={query_params.get("qH")[0]}'
+    if matched_element:
+      flipkart_product_url = "https://www.flipkart.com" + matched_element['href']
       
+      # print(flipkart_product_url)
       
-    flipkart_product_response = requests.get(flipkart_parsed_url, headers= custom_headers)
-    flipkart_product_soup = BeautifulSoup(flipkart_product_response.text, 'lxml')
+      parsed_url = urlparse(flipkart_product_url)
+      query_params = parse_qs(parsed_url.query)
+      base_url = f"{parsed_url.scheme}://{parsed_url.netloc}{parsed_url.path}"
+      flipkart_parsed_url = f'{base_url}?q={encoded_term}'
+      
+      if query_params.get("spotlightTagId"):
+        flipkart_parsed_url += f'&spotlightTagId={query_params.get("spotlightTagId")[0]}'
 
-    file_path = "flipkart_product_response.html"
-    # Open the file in write mode
-    with open(file_path, 'w', encoding='utf-8') as file:
-        file.write(flipkart_product_soup.prettify())
+      if query_params.get("qH"):
+        flipkart_parsed_url += f'&qH={query_params.get("qH")[0]}'
         
-    flip_product = {}
-    
-    flip_product["name"] = flipkart_product_soup.find("span", class_="B_NuCI").text
-    flip_product["price"] = flipkart_product_soup.find("div", class_="_30jeq3 _16Jk6d").text
-    flip_product["rating"] = flipkart_product_soup.find("div", class_="_3LWZlK").text
-    flip_product["image"] = flipkart_product_soup.find("img", class_="_396cs4 _2amPTt _3qGmMb")['src']
-    flip_product["link"] = flipkart_parsed_url
-    
-    return flip_product
+        
+      flipkart_product_response = requests.get(flipkart_parsed_url, headers= custom_headers)
+      flipkart_product_soup = BeautifulSoup(flipkart_product_response.text, 'lxml')
+
+      file_path = "flipkart_product_response.html"
+      # Open the file in write mode
+      with open(file_path, 'w', encoding='utf-8') as file:
+          file.write(flipkart_product_soup.prettify())
+          
+      flip_product = {}
+      
+      flip_product["name"] = flipkart_product_soup.find("span", class_="B_NuCI").text
+      flip_product["price"] = flipkart_product_soup.find("div", class_="_30jeq3 _16Jk6d").text
+      flip_product["rating"] = flipkart_product_soup.find("div", class_="_3LWZlK").text
+      flip_product["image"] = flipkart_product_soup.find("img", class_="_396cs4 _2amPTt _3qGmMb")['src']
+      flip_product["link"] = flipkart_parsed_url
+      
+      return flip_product
     
     
   except Exception as e:
@@ -170,6 +175,8 @@ def scrape_md(encoded_term):
           md_product["link"] = md_product_url
           
           return md_product
+    else:
+      return {}
   
   except Exception as e:
     st.write("Error: ", e)
@@ -222,7 +229,7 @@ def scrape_vedant(encoded_term):
       img_container = vedant_product_soup.find('div', class_='swiper-slide')
       img = img_container.find('img')
       if img:
-        vedant_product["image"] = img['src']
+        vedant_product["image"] = img['data-cfsrc']
         
       vedant_product["link"] = vedant_product_url
       
