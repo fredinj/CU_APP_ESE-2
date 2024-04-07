f629199068fd2bac672cffa9926fc50f8a205cf8
Fredin <90355410+fredinj@users.noreply.github.com>
2024-04-08 00:22:08 +0530

    Add mdcomputers and prime abgb

--- app.py
+++ app.py
@@ -4,8 +4,15 @@ import urllib.parse
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent as ua
 from urllib.parse import urlparse, parse_qs
+import re
 # from time import sleep
-import math
+# import math
+
+# adjustable search number limit on sidebar
+# save for analysis button
+# make analysis
+# sort products based on price
+#  maybe (scatter) plots based on price and different colors for different stores
  
  
   
@@ -17,7 +24,7 @@ def main():
     if len(search_term.strip()) != 0:
       with st.spinner('Loading data...'):
         # sleep(5)  # Sleep for 1 second
-        scrape_market(search_term.strip())
+        scrape_markets(search_term.strip())
     else:
       st.error("Enter a valid search term")
     
@@ -84,8 +91,8 @@ def scrape_flipkart(encoded_term):
   except Exception as e:
     st.write("Error: ", e)
   
-  
-def scrape_md(encoded_term):
+
+def scrape_md(encoded_term, item_count):
   ua_fake = ua().random
   custom_headers = {"User-Agent":ua_fake, 
                   "Accept-Encoding":"gzip, deflate", 
@@ -103,56 +110,45 @@ def scrape_md(encoded_term):
     file_path = "md_search_response.html"
     # Open the file in write mode
     with open(file_path, 'w', encoding='utf-8') as file:
-        file.write(md_search_soup.prettify())
+      file.write(md_search_soup.prettify())
+      
+    products_container_list = md_search_soup.find('div', class_="products-list")
+    if not products_container_list:
+      return []
+
+    products_list_inner = products_container_list.find_all('div', class_="product-layout", recursive=False)
+
+    products_list_inner = [i.find('div', class_="product-item-container", recursive=False) for i in products_list_inner]
+  
+  
+    products_list=[]
+
+    for i in products_list_inner:
+      product={}
+      
+      product['name'] = i.find('div', class_="right-block").find('h4').find('a').text.strip()
+      product['link'] = i.find('div', class_="right-block").find('h4').find('a')['href']
+      product['price'] = i.find('div', class_='right-block').find('div', class_="price").find('span').text.strip()
+      
+      if i.find('div', class_="left-block").find('div', class_='product-image-container').find('a').find('img').has_attr('data-src'):
+        product['image'] = "https:" + i.find('div', class_="left-block").find('div', class_='product-image-container').find('a').find('img')['data-src']
+      elif i.find('div', class_="left-block").find('div', class_='product-image-container').find('a').find('img').has_attr('src'):
+        product['image'] = "https:" + i.find('div', class_="left-block").find('div', class_='product-image-container').find('a').find('img')['src']
+      else:
+        product['image'] = ""
+      
+      products_list.append(product)
+      
+      if len(products_list) >= item_count:
+        break
     
-    product_image_container = md_search_soup.find('div', class_='product-image-container')
-    if product_image_container:
-      a_tag = product_image_container.find('a')
-      if a_tag:
-        # Extract the text within the a tag
-        md_product_url = a_tag['href']
-
-        # matched_element = md_search_soup.find('div', href=lambda href: href and "/p/" in href)
-    
-        md_product_response = requests.get(md_product_url, headers= custom_headers)
-        md_product_soup = BeautifulSoup(md_product_response.text, 'lxml')
-
-        file_path = "md_product_response.html"
-        # Open the file in write mode
-        with open(file_path, 'w', encoding='utf-8') as file:
-            file.write(md_product_soup.prettify())
-            
-            
-        md_product = {}
-
-        md_product["name"] = md_product_soup.find("span", class_="more_show")["data-full_title"]
-        md_product["price"] = md_product_soup.find("span", id="price-special").text
-
-        rating_div = md_product_soup.find("span", class_="rating-point")
-        if rating_div:
-          md_product["rating"] = rating_div.text
-        # md_product["rating"] = md_product_soup.find("span", class_="rating-point").text
-
-        image_div = md_product_soup.find("div", class_="large-image")
-        if image_div:
-          md_product["image"] = "https:"+image_div.find("img")['src']
-        # md_product["image"] = "https:"+md_product_soup.find("img", class_="large-image  ")['src']
-
-        md_product["link"] = md_product_url
-        
-        return md_product
-    else:
-      return {}
+    return products_list
   
   except Exception as e:
     st.write("Error: ", e)
-
-
-def scrape_vedant(encoded_term):
   
-  # get from setup later
-  item_count = 4  
-  
+
+def scrape_vedant(encoded_term, item_count):  
   ua_fake = ua().random
   custom_headers = {"User-Agent":ua_fake, 
                   "Accept-Encoding":"gzip, deflate", 
@@ -189,10 +185,14 @@ def scrape_vedant(encoded_term):
       for i in product_div:
         product = {}
         product["name"] = i.find('div', class_='product-thumb').find('div', class_='caption').find('div', class_="name").find('a').text.strip()
+        
         if i.find('div', class_='product-thumb').find('div', class_='caption').find('div', class_="price").find('span', class_="price-new"):
           product["price"] = i.find('div', class_='product-thumb').find('div', class_='caption').find('div', class_="price").find('span', class_="price-new").text.strip()
-        else:
+        elif i.find('div', class_='product-thumb').find('div', class_='caption').find('div', class_="price").find('span', class_="price-normal"):
           product["price"] = i.find('div', class_='product-thumb').find('div', class_='caption').find('div', class_="price").find('span', class_="price-normal").text.strip()
+        else:
+          product["price"] = "N/A"
+          
         product["link"] = i.find('div', class_='product-thumb').find('div', class_='caption').find('div', class_="name").find('a')['href']
         
         img_src_set = i.find('div', class_='product-thumb').find('div', class_='image').find('a', class_="product-img").find('div').find('img')['data-srcset']
@@ -215,9 +215,64 @@ def scrape_vedant(encoded_term):
     st.write("Error: ", e)
     
 
+def scrape_prime(encoded_term, item_count):
+  ua_fake = ua().random
+  custom_headers = {"User-Agent":ua_fake, 
+                  "Accept-Encoding":"gzip, deflate", 
+                  "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                  "DNT":"1",
+                  "Connection":"close", 
+                  "Upgrade-Insecure-Requests":"1"}
+
+  prime_search_url = f'https://www.primeabgb.com/?s={encoded_term}&post_type=product'
+
+  try:
+    prime_search_response = requests.get(prime_search_url, headers= custom_headers)
+    prime_search_soup = BeautifulSoup(prime_search_response.text, 'html.parser')
+
+    file_path = "prime_search_response.html"
+    # Open the file in write mode
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(prime_search_soup.prettify())
+        
+    site_wrapper = prime_search_soup.find('div', class_='site-wrapper')
+
+    primary_div = site_wrapper.find('div', id='main-content').find('div', class_='container').find('div', class_='row').find('div', id="primary")
+
+    if primary_div.find('div', class_="bapf_no_products"):
+      return []
+
+    products_grid = primary_div.find('div', class_="products", recursive=False).find_all('div', class_="product", recursive=False)
+
+    products_list = []
+
+    for i in products_grid:
+      product = {}
+      product['name'] = i.find('div', class_="product-wrapper").find('div', class_="product-info").find("div", class_="product-title-rating").find("h3", class_="product-title").find('a').text.strip()
+      product['link'] = i.find('div', class_="product-wrapper").find('div', class_="product-info").find("div", class_="product-title-rating").find("h3", class_="product-title").find('a')['href']
+      
+      
+      if i.find('div', class_="product-wrapper").find('div', class_="product-info").find("div", class_="product-price-buttons").find('div', class_="product-price").find('span', class_="woocommerce-Price-amount amount"):
+        product['price'] = i.find('div', class_="product-wrapper").find('div', class_="product-info").find("div", class_="product-price-buttons").find('div', class_="product-price").find('span', class_="woocommerce-Price-amount amount").find('bdi').text.strip()
+      else:
+        product['price'] = "N/A"
+      
+      product['image'] = i.find('div', class_="product-wrapper").find('div', class_="product-image").find('a').find('img')['src']
+      
+      products_list.append(product)
+      
+      if len(products_list) >= item_count:
+        break
+      
+    return products_list
+  
+  except Exception as e:
+    st.write("Error: ", e)
+
+
 def card(product, col):
     content = f"""
-    <div class="product-card">
+    <div class="product-card" style="margin: 20px;">
         <img src="{product['image']}" alt="Product Image">
         <div class="product-details">
             <h2><a href="{product['link']}">{product['name']}</a></h2>
@@ -228,7 +283,10 @@ def card(product, col):
     col.markdown(content, unsafe_allow_html=True)
 
 
-def scrape_market(search_term):
+def scrape_markets(search_term):
+  
+  # get from setup later
+  item_count = 4  
   
   encoded_term = urllib.parse.quote_plus(search_term)
   
@@ -242,9 +300,17 @@ def scrape_market(search_term):
   # if md_product and len(md_product)>0:  
   #   market_list.append(md_product)
   
-  vendant_list = scrape_vedant(encoded_term)
+  vendant_list = scrape_vedant(encoded_term, item_count)
   if vendant_list and len(vendant_list)>0:
     scrape_results['vedant'] = vendant_list
+    
+  md_list = scrape_md(encoded_term, item_count)
+  if md_list and len(md_list)>0:
+    scrape_results['md'] = md_list
+    
+  prime_list = scrape_prime(encoded_term, item_count)
+  if prime_list and len(prime_list)>0:
+    scrape_results['prime'] = prime_list
 
   
   parse_data(scrape_results)
@@ -256,7 +322,6 @@ def parse_data(scrape_results):
     item_count = 0
 
     vedant_expander = st.expander("Vedant Computers", expanded=True)
-
     with vedant_expander:
       # st.markdown("---")
       if 'vedant' in scrape_results:
@@ -268,11 +333,46 @@ def parse_data(scrape_results):
                 card(scrape_results['vedant'][i + j], vedant_col[j])
                 item_count += 1
                 if item_count >= 2:
-                  st.markdown("""<div style="margin-bottom: 20px;"></div>""", unsafe_allow_html=True)
+                  # st.markdown("""<div style="margin-bottom: 20px;"></div>""", unsafe_allow_html=True)
                   item_count = 0
       else:
           st.error("No products found on Vedant Computers")
+          
+          
+    md_expander = st.expander("MD Computers", expanded=True)
+    with md_expander:
+      # st.markdown("---")
+      if 'md' in scrape_results:
+        for i in range(0, len(scrape_results['md']), items_per_row):
+          with st.container():
+            md_col = st.columns(items_per_row)
+            for j in range(items_per_row):
+              if i + j < len(scrape_results['md']):
+                card(scrape_results['md'][i + j], md_col[j])
+                item_count += 1
+                if item_count >= 2:
+                  # st.markdown("""<div style="margin-bottom: 20px;"></div>""", unsafe_allow_html=True)
+                  item_count = 0
+      else:
+          st.error("No products found on MD Computers")
 
+    
+    prime_expander = st.expander("PrimeABGB", expanded=True)
+    with prime_expander:
+      # st.markdown("---")
+      if 'prime' in scrape_results:
+        for i in range(0, len(scrape_results['prime']), items_per_row):
+          with st.container():
+            prime_col = st.columns(items_per_row)
+            for j in range(items_per_row):
+              if i + j < len(scrape_results['prime']):
+                card(scrape_results['prime'][i + j], prime_col[j])
+                item_count += 1
+                if item_count >= 2:
+                  # st.markdown("""<div style="margin-bottom: 20px;"></div>""", unsafe_allow_html=True)
+                  item_count = 0
+      else:
+          st.error("No products found on PrimeABGB")
 
 
 if __name__ == "__main__":
